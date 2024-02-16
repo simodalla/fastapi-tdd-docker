0dc3f237478796a6a5d4927710f299bb1894a4cb
Simone Dalla <simodalla@gmail.com>
2024-02-16 22:01:39 +0100

    pytest setup

---
+++ project/tests/conftest.py
@@ -0,0 +1,17 @@
+import os
+
+import pytest
+from app import main
+from app.config import Settings, get_settings
+from starlette.testclient import TestClient
+
+
+def get_settings_override():
+    return Settings(testing=1, database_url=os.environ.get("DATABASE_TEST_URL"))
+
+
+@pytest.fixture(scope="module")
+def test_app():
+    main.app.dependency_overrides[get_settings] = get_settings_override
+    with TestClient(main.app) as test_client:
+        yield test_client

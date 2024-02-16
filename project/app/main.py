9ab2d482d9244182efcf38802b5a0cb62ce37910
Simone Dalla <simodalla@gmail.com>
2024-02-16 18:30:03 +0100

    postgres setup, tortoise and aerich

--- project/app/main.py
+++ project/app/main.py
@@ -1,8 +1,19 @@
+import os
+
 from fastapi import Depends, FastAPI
+from tortoise.contrib.fastapi import register_tortoise
 
 from app.config import Settings, get_settings
 
 app = FastAPI()
+
+register_tortoise(
+    app,
+    db_url=os.environ.get("DATABASE_URL"),
+    modules={"models": ["app.models.tortoise"]},
+    generate_schemas=False,
+    add_exception_handlers=True,
+)
 
 
 @app.get("/ping")

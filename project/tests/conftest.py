493a2ff0ef3a20336ea36453a8807901c6a8279d
Simone Dalla <simodalla@gmail.com>
2024-02-17 12:08:13 +0100

    app structure - database init

--- project/tests/conftest.py
+++ project/tests/conftest.py
@@ -1,9 +1,10 @@
 import os
 
 import pytest
-from app import main
-from app.config import Settings, get_settings
 from starlette.testclient import TestClient
+
+from app.config import Settings, get_settings
+from app.main import create_application
 
 
 def get_settings_override():
@@ -12,6 +13,7 @@ def get_settings_override():
 
 @pytest.fixture(scope="module")
 def test_app():
-    main.app.dependency_overrides[get_settings] = get_settings_override
-    with TestClient(main.app) as test_client:
+    app = create_application()
+    app.dependency_overrides[get_settings] = get_settings_override
+    with TestClient(app) as test_client:
         yield test_client
